8348a597ae7694b7fb0c9e706770c8a37e844dbb
jean <jeancs.patricio@gmail.com>
2019-11-12 17:40:58 -0300

    WIP: Removendo MIM

--- todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/ViewModelHolder.java
+++ todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/ViewModelHolder.java
@@ -18,7 +18,7 @@ package com.example.android.architecture.blueprints.todoapp;
 
 
 import android.os.Bundle;
-import androidx.annotation.NonNull;
+
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
@@ -26,29 +26,11 @@ import androidx.fragment.app.Fragment;
 /**
  * Non-UI Fragment used to retain ViewModels.
  */
-public class ViewModelHolder<VM> extends Fragment {
-
-    private VM mViewModel;
-
-    public ViewModelHolder() { }
-
-    public static <M> ViewModelHolder createContainer(@NonNull M viewModel) {
-        ViewModelHolder<M> viewModelContainer = new ViewModelHolder<>();
-        viewModelContainer.setViewModel(viewModel);
-        return viewModelContainer;
-    }
+public class ViewModelHolder extends Fragment {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
-    }
-
-    @Nullable public VM getViewmodel() {
-        return mViewModel;
-    }
-
-    public void setViewModel(@NonNull VM viewModel) {
-        mViewModel = viewModel;
     }
 }
